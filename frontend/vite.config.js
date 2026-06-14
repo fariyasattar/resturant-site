259e71045d3b4da46a3107221ebddaa213eac0c0
fariyasattar <fariyasattar8899@gmail.in>
2026-06-14 06:06:19 -0700

    fix: remove duplicate base patch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-
+    base: '/',
   plugins: [react(),
     tailwindcss(),
    
   ],
-  base: '/resturant-site/'
+ 
 })
